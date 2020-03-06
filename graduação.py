d302391be6f11a0797f86f5fb3a5ebd90ef3380f
luizfelipe564 <61593278+luizfelipe564@users.noreply.github.com>
2020-03-06 19:04:56 -0300

    Add files via upload

---
+++ graduação.py
@@ -0,0 +1,8 @@
+graduacao = input("que tipo de graduação voce realiza: ").upper()
+if graduacao != "TECNOLOGO" and graduacao != "BACHARELADO":
+    graduacao = input ("digite uma graduação valida: ").upper()
+if graduacao == "TECNOLOGO":
+        print("2 a 3 anos")
+
+else :
+    print("4 até 6 anos")
