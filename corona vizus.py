6736dc1404302ddc4e93ff5418f1560e291d919b
luizfelipe564 <61593278+luizfelipe564@users.noreply.github.com>
2020-03-06 17:29:07 -0300

    Add files via upload

---
+++ corona vizus.py
@@ -0,0 +1,17 @@
+nome = input("nome do paciente: ")
+idade = int (input("digite sua idade: "))
+
+if idade < 15 or idade > 60:
+    print( " procure acesso prioritario " )
+    exit()
+
+risco = input(",esta em grupo de risco sim ou não:").upper()
+
+if risco != "SIM" and risco != "NAO":
+    risco = input("digite apenas sim ou não: ").upper()
+
+if risco == "SIM":
+    print("procure acesso prioritario")
+
+else:
+    print("acesse a fila normal")
